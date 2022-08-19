3959c7aeea923ad4f361a35c3d91d4e67addd61d
Justin Alvarez <justinalvarez.1221@gmail.com>
2022-08-19 11:22:13 -0500

    fourth commit complete routes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const Port = 3000
+
+
+app.get('/hello', function(req, res){
+    res.send('this is working')
+})
+app.get('/', function(req, res){
+    res.send('home page')
+})
+
+app.post('/', function(req, res){
+    res.send('post route is working')
+
+})
+
+
+app.listen(3000, function(){
+    console.log(`listening on port: ${Port}`)
+})
